cb68d1c553154f44b90cb6987ae848a9e3e5a4c0
João Eduardo Marques Salles <dudumarques211299@gmail.com>
2023-05-11 13:13:21 -0300

    Update

--- js/calculadora.js
+++ js/calculadora.js
@@ -17,3 +17,91 @@ onload = () => {
     document.querySelector('#bt-dell').onclick = limpa;
     document.querySelector('#bt-ig').onclick = calcula;
 }
+// Variáveis para armazenarmos o valor, o operador e o estado da calculadora
+let sValor = '0'; // valor que será apresentado no display
+let ehNovoNumero = true; // Indica se o próximo digito será de um novo número
+let valorAnterior = 0; //  Valor acumulado para uma operação
+let operacaoPendente = null; // Operação acumulada
+
+// Atualização do visor
+const atualizaVisor = () => {
+  let [parteInteira, parteDecimal] = sValor.split(',');
+  if (parteInteira.length > 10) {
+    document.querySelector('#display').innerText = 'Erro';
+    return;
+  }
+  let v = '';
+  c = 0;
+  for (let i = parteInteira.length - 1; i >= 0; i--) {
+    if (++c > 3) {
+      v = '.' + v;
+      c = 1;
+    }
+    v = parteInteira[i] + v;
+  }
+  v = v + (parteDecimal ? ',' + parteDecimal.substr(0, 10 - v.length) : '');
+  document.querySelector('#display').innerText = v;
+};
+
+// Tratamento do clique no botão de dígito
+const digito = (n) => {
+  if (ehNovoNumero) {
+    sValor = '' + n;
+    ehNovoNumero = false;
+  } else sValor += n;
+  atualizaVisor();
+};
+
+// Tratamento do clique no botão de ponto decimal
+const virgula = () => {
+  if (ehNovoNumero) {
+    sValor = '0,';
+    ehNovoNumero = false;
+  } else if (sValor.indexOf(',') == -1) sValor += ',';
+  atualizaVisor();
+};
+
+// Tratamento do clique no botão AC (All Clear)
+const limpa = () => {
+  ehNovoNumero = true;
+  valorAnterior = 0;
+  sValor = '0';
+  operacaoPendente = null;
+  atualizaVisor();
+};
+
+// Converte a string do valor para um número real
+const valorAtual = () => parseFloat(sValor.replace(',', '.'));
+
+// Tratamento do clique nos botões de operadores
+const operador = (op) => {
+  calcula();
+  valorAnterior = valorAtual();
+  operacaoPendente = op;
+  ehNovoNumero = true;
+};
+
+const calcula = () => {
+  if (operacaoPendente != null) {
+    let resultado;
+    switch (operacaoPendente) {
+      case '+':
+        resultado = valorAnterior + valorAtual();
+        break;
+      case '-':
+        resultado = valorAnterior - valorAtual();
+        break;
+      case '*':
+        resultado = valorAnterior * valorAtual();
+        break;
+      case '/':
+        resultado = valorAnterior / valorAtual();
+        break;
+    }
+    sValor = resultado.toString().replace('.', ',');
+  }
+  ehNovoNumero = true;
+  operacaoPendente = null;
+  valorAnterior = 0;
+  atualizaVisor();
+};
